01bcf7c637858c05636290ff78d64e064bf882cc
Aleksej0606 <prihodkoaleksej01@gmail.com>
2025-04-07 19:33:29 +0500

    неделя 2 добавил передвижение

--- RidingGame/Form1.Designer.cs
+++ RidingGame/Form1.Designer.cs
@@ -28,60 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox14 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            this.bg1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bg2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // bg1
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1024, 1024);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.bg1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bg1.BackgroundImage")));
+            this.bg1.Location = new System.Drawing.Point(0, 0);
+            this.bg1.Name = "bg1";
+            this.bg1.Size = new System.Drawing.Size(1024, 1024);
+            this.bg1.TabIndex = 0;
+            this.bg1.TabStop = false;
+            this.bg1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // pictureBox14
+            // player
             // 
-            this.pictureBox14.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox14.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox14.BackgroundImage")));
-            this.pictureBox14.Location = new System.Drawing.Point(418, 738);
-            this.pictureBox14.Name = "pictureBox14";
-            this.pictureBox14.Size = new System.Drawing.Size(192, 224);
-            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox14.TabIndex = 13;
-            this.pictureBox14.TabStop = false;
-            this.pictureBox14.Click += new System.EventHandler(this.pictureBox14_Click);
+            this.player.BackColor = System.Drawing.Color.DimGray;
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.Location = new System.Drawing.Point(455, 729);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(110, 223);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 13;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.pictureBox14_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bg2
+            // 
+            this.bg2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bg2.BackgroundImage")));
+            this.bg2.Location = new System.Drawing.Point(0, -1024);
+            this.bg2.Name = "bg2";
+            this.bg2.Size = new System.Drawing.Size(1024, 1024);
+            this.bg2.TabIndex = 14;
+            this.bg2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GrayText;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1024, 1024);
-            this.Controls.Add(this.pictureBox14);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bg1);
+            this.Controls.Add(this.bg2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox bg1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox bg2;
     }
 }
 
--- RidingGame/Form1.cs
+++ RidingGame/Form1.cs
@@ -25,17 +25,44 @@ namespace RidingGame
             }
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int speed = 14;
+            bg1.Top += speed;
+            bg2.Top += speed;
+
+            if (bg1.Top >= 1005)
+            {
+                bg1.Top = 0;
+                bg2.Top = -1005;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speed = 10;
+            if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && player.Left > 24)
+            {
+                player.Left -= speed;
+            }
+
+            else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && player.Right < 1000)
+            {
+                player.Left += speed;
+            }
+
+            else if ((e.KeyCode == Keys.Up || e.KeyCode == Keys.W) && player.Top > 0)
+            {
+                player.Top -= speed;
+            }
+
+            else if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.S) && player.Top < 800)
+            {
+                player.Top += speed;
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox8_Click(object sender, EventArgs e)
         {
 
         }
